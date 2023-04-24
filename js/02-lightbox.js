d9fbd8f3df5c2352a9b51b79f7542e8bb9ba962b
LaPaNu4 <120036931+LaPaNu4@users.noreply.github.com>
2023-04-24 22:34:17 +0300

    2nd

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
+const galleryEL = document.querySelector(".gallery");
+const galleryItemEl = galleryItems
+  .map(
+    (item) => `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`
+  )
+  .join("");
+galleryEL.innerHTML = galleryItemEl;
+galleryEL.addEventListener("click", onGalleryItemClick);
+function onGalleryItemClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const options = {
+    captionsData: "alt",
+    captionDelay: 250,
+  };
+  const lightbox = new SimpleLightbox(".gallery a", options);
+  galleryEL.removeEventListener("click", onGalleryItemClick);
+}
